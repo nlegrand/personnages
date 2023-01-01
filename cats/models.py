4c6e2218c93226752c0bc9a3d55f9de0ec73ba5f
Nicolas Legrand <nicolas.legrand@gmail.com>
2023-01-01 22:44:59 +0100

    List distributed favors
    change schema to meet the need.

--- cats/models.py
+++ cats/models.py
@@ -6,6 +6,16 @@ class Game(models.Model):
     name = models.CharField(max_length=200)
     def __str__(self):
         return self.name
+class Favor(models.Model):
+    how_many = models.IntegerField()
+    title = models.CharField(max_length=50)
+    affect = models.CharField(max_length=50)
+    text = models.CharField(max_length=200)
+    warning = models.CharField(max_length=100, blank=True)
+    def __str__(self):
+        return self.title
+    def distributed_favors(game_id):
+        return Favor.objects.filter(character__game__id=game_id)
 
 class Character(models.Model):
     type = [('CAT', 'cat'),('HUMAN', 'human'),('BASTET', 'bastet')]
@@ -16,20 +26,11 @@ class Character(models.Model):
     reputation = models.CharField(max_length=200)
     faction = models.CharField(max_length=200)
     owner = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    game = models.ManyToManyField(Game)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    favor = models.ManyToManyField(Favor)
     def __str__(self):
         return self.name
     
-class Favor(models.Model):
-    character = models.ManyToManyField(Character, blank=True)
-    how_many = models.IntegerField()
-    title = models.CharField(max_length=50)
-    affect = models.CharField(max_length=50)
-    text = models.CharField(max_length=200)
-    warning = models.CharField(max_length=100, blank=True)
-    def __str__(self):
-        return self.title
-
 class Advantage(models.Model):
     character = models.ForeignKey(Character, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
